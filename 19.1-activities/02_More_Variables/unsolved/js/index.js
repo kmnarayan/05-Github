3b12021d58a9cc54ba01721651ba6e896d9e8248
Karen Narayan <90415945+kmnarayan@users.noreply.github.com>
2022-02-08 10:22:18 -0500

    adding 19.1 homework

--- 19.1-activities/02_More_Variables/unsolved/js/index.js
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -1,3 +1,4 @@
 // Create the addNumbers function here.
+function addNumbers()
 
 // Create the stringConcat function here.
--- 19.1-activities/04_on_function/unsolved/js/index.js
+++ 19.1-activities/04_on_function/unsolved/js/index.js
@@ -1,8 +1,8 @@
 // .on event listener documentation - https://api.jquery.com/on/#on-events-selector-data-handler
 
 // Step 1: Hide a div
-$(" ").on(" ", function(){
-
+$("buton1").on("click", function(){
+    $('hideMe').slideToggle();
 });
 
 // Step 2: Change a color
