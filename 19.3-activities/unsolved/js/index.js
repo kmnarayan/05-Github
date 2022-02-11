386adaeb5396b6d17121bc7b3e1df91706f4d2ec
Karen Narayan <90415945+kmnarayan@users.noreply.github.com>
2022-02-10 20:27:11 -0500

    19.3 activity

--- 19.3-activities/unsolved/js/index.js
+++ 19.3-activities/unsolved/js/index.js
@@ -1,16 +1,27 @@
+function fade() {
+  $('.preloader').fadeOut("slow");
+  }
+  setTimeout(fade, 3000);
+
 $(".searchButton").on("click", function(){
+$(".searchButton").on("click", function(){
+
+
   $(".searchButton").toggleClass("active");
   if ($(".searchButton").hasClass("active")) {
     $(".searchBar").css("height", "100vh");
     $("#searchForm").css("opacity","1");
     $(".searchIcon").removeClass("fa-search").addClass("fa-times");
   }
-  else {
-    $(".searchBar").css('height', "0vh");
-    $("#searchForm").css("opacity","0");
-    $(".searchIcon").removeClass("fa-times").addClass("fa-search");
-  }
+else{
+
+}
+
+
+
 });
+
+
 
 
 
